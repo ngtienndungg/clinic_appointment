6a9af539cc3eafd4466b8d542449efa45f8694ef
ngtienndungg <dungnt.works@gmail.com>
2023-09-18 16:00:21 +0700

    configuration: retrofit client

---
+++ app/src/main/java/com/example/clinic_appointment/networking/AppointmentService.java
@@ -0,0 +1,5 @@
+package com.example.clinic_appointment.networking;
+
+public class AppointmentService {
+
+}
---
+++ app/src/main/java/com/example/clinic_appointment/networking/RetrofitClient.java
@@ -0,0 +1,19 @@
+package com.example.clinic_appointment.networking;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit = null;
+    private static final String BASE_URL = "http://localhost:5000/";
+
+    public static AppointmentService getService() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(AppointmentService.class);
+    }
+}
