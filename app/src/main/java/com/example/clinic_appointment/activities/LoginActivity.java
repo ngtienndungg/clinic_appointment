95a162f99dbe60939899c66b4ef58fd61e304301
ngtienndungg <dungnt.works@gmail.com>
2023-09-18 10:15:53 +0700

    user interface: simple login screen for testing API

--- app/src/main/java/com/example/clinic_appointment/activities/LoginActivity.java
+++ app/src/main/java/com/example/clinic_appointment/activities/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.clinic_appointment;
+package com.example.clinic_appointment.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.clinic_appointment.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
