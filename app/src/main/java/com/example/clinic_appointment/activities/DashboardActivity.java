606cc1c0b9155772a54d9e94da8a7a30b39cf234
ngtienndungg <dungnt.works@gmail.com>
2023-10-06 16:44:57 +0700

    feature: item selecting in bottom navigation bar

--- app/src/main/java/com/example/clinic_appointment/activities/DashboardActivity.java
+++ app/src/main/java/com/example/clinic_appointment/activities/DashboardActivity.java
@@ -1,16 +1,83 @@
 package com.example.clinic_appointment.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.clinic_appointment.R;
+import com.example.clinic_appointment.databinding.ActivityDashboardBinding;
+import com.example.clinic_appointment.fragments.AccountFragment;
+import com.example.clinic_appointment.fragments.HomeFragment;
+import com.example.clinic_appointment.fragments.MyScheduleFragment;
+import com.example.clinic_appointment.fragments.ScheduleFragment;
 
 public class DashboardActivity extends AppCompatActivity {
+    private ActivityDashboardBinding binding;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initiate();
+        eventHandling();
     }
+
+    private void initiate() {
+        currentFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, currentFragment).commit();
+    }
+
+
+    @SuppressLint("NonConstantResourceId")
+    private void eventHandling() {
+        binding.bnvDashboard.setOnItemSelectedListener(item -> {
+            Fragment newFragment = null;
+            switch (item.getItemId()) {
+                case R.id.menu_item_home:
+                    newFragment = new HomeFragment();
+                    break;
+                case R.id.menu_item_schedule:
+                    newFragment = new ScheduleFragment();
+                    break;
+                case R.id.menu_item_my_schedule:
+                    newFragment = new MyScheduleFragment();
+                    break;
+                case R.id.menu_item_account:
+                    newFragment = new AccountFragment();
+                    break;
+            }
+
+            if (newFragment != null && !(currentFragment.getClass().equals(newFragment.getClass()))) {
+                replaceFragment(newFragment);
+                return true;
+            }
+
+            return false;
+        });
+    }
+
+    private void replaceFragment(Fragment newFragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, newFragment)
+                .commit();
+        currentFragment = newFragment;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!(currentFragment instanceof HomeFragment)) {
+            binding.bnvDashboard.setSelectedItemId(R.id.menu_item_home);
+            currentFragment = new HomeFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, currentFragment).commit();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
