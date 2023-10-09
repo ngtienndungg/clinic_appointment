f2caf155ac64f0bba190e458da4dd40bce56e7db
ngtienndungg <dungnt.works@gmail.com>
2023-10-09 10:42:24 +0700

    model: address

---
+++ app/src/main/java/com/example/clinic_appointment/models/Address/Address.java
@@ -0,0 +1,36 @@
+package com.example.clinic_appointment.models.Address;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Address {
+    @SerializedName("province")
+    private String province;
+    @SerializedName("district")
+    private String district;
+    @SerializedName("ward")
+    private String ward;
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getWard() {
+        return ward;
+    }
+
+    public void setWard(String ward) {
+        this.ward = ward;
+    }
+}
