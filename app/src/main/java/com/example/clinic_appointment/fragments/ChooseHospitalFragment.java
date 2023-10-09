155232ffe53725870b4b12274ff72f59de3fe18c
ngtienndungg <dungnt.works@gmail.com>
2023-10-09 10:22:31 +0700

    user interface: choose hospital fragment

---
+++ app/src/main/java/com/example/clinic_appointment/fragments/ChooseHospitalFragment.java
@@ -0,0 +1,23 @@
+package com.example.clinic_appointment.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.clinic_appointment.R;
+
+public class ChooseHospitalFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_choose_hospital, container, false);
+    }
+}
