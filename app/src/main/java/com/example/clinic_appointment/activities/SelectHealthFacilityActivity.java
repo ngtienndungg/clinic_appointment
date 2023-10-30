3e96e8bfa6cb9df072ab393fd0b0fded55d52257
ngtienndungg <dungnt.works@gmail.com>
2023-10-30 21:16:48 +0700

    user interface: select health facility

---
+++ app/src/main/java/com/example/clinic_appointment/activities/SelectHealthFacilityActivity.java
@@ -0,0 +1,16 @@
+package com.example.clinic_appointment.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.clinic_appointment.R;
+
+public class SelectHealthFacilityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_health_facility);
+    }
+}
