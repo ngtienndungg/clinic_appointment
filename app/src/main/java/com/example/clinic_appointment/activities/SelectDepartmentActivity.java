f7f839c40f431081661ce661b5aa1577c9984b36
ngtienndungg <dungnt.works@gmail.com>
2023-10-31 10:37:10 +0700

    feature: display list of departments

--- app/src/main/java/com/example/clinic_appointment/activities/SelectDepartmentActivity.java
+++ app/src/main/java/com/example/clinic_appointment/activities/SelectDepartmentActivity.java
@@ -1,16 +1,64 @@
 package com.example.clinic_appointment.activities;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.clinic_appointment.adapters.SelectDepartmentAdapter;
+import com.example.clinic_appointment.databinding.ActivitySelectDepartmentBinding;
+import com.example.clinic_appointment.listeners.DepartmentListener;
+import com.example.clinic_appointment.models.Department.Department;
+import com.example.clinic_appointment.models.Department.DepartmentResponse;
+import com.example.clinic_appointment.networking.clients.RetrofitClient;
 
-import com.example.clinic_appointment.R;
+import java.util.List;
 
-public class SelectDepartmentActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SelectDepartmentActivity extends AppCompatActivity implements DepartmentListener {
+
+    private ActivitySelectDepartmentBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_department);
+        binding = ActivitySelectDepartmentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initiate();
+    }
+
+    private void initiate() {
+        Call<DepartmentResponse> call = RetrofitClient.getPublicAppointmentService().getFilteredDepartment();
+        call.enqueue(new Callback<DepartmentResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<DepartmentResponse> call, @NonNull Response<DepartmentResponse> response) {
+                if (response.body() != null && response.body().isSuccess()) {
+                    List<Department> departments = response.body().getDepartments();
+                    SelectDepartmentAdapter adapter = new SelectDepartmentAdapter(SelectDepartmentActivity.this, departments);
+                    binding.rvDepartment.setAdapter(adapter);
+                    binding.rvDepartment.setVisibility(View.VISIBLE);
+                    binding.pbLoading.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<DepartmentResponse> call, @NonNull Throwable t) {
+                displayError();
+            }
+        });
+    }
+
+    private void displayError() {
+        binding.pbLoading.setVisibility(View.GONE);
+        binding.rlError.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onClick(Department department) {
+
     }
 }
---
+++ app/src/main/java/com/example/clinic_appointment/listeners/DepartmentListener.java
@@ -0,0 +1,8 @@
+package com.example.clinic_appointment.listeners;
+
+import com.example.clinic_appointment.models.Department.Department;
+import com.example.clinic_appointment.models.HealthFacility.HealthFacility;
+
+public interface DepartmentListener {
+    void onClick(Department department);
+}
