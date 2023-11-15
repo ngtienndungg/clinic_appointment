1485f703fea66e0e8a31d03e9e4ce31fefbdab0c
ngtienndungg <dungnt.works@gmail.com>
2023-11-15 15:45:25 +0700

    update: local date gson converter

--- app/src/main/java/com/example/clinic_appointment/networking/clients/RetrofitClient.java
+++ app/src/main/java/com/example/clinic_appointment/networking/clients/RetrofitClient.java
@@ -2,6 +2,13 @@ package com.example.clinic_appointment.networking.clients;
 
 import com.example.clinic_appointment.networking.interceptors.CurrentSessionInterceptor;
 import com.example.clinic_appointment.networking.services.AppointmentService;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -37,9 +44,22 @@ public class RetrofitClient {
             publicRetrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .client(client)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
+                            .registerTypeAdapter(LocalDate.class, new LocalDateAdapter().nullSafe()).create()))
                     .build();
         }
         return publicRetrofit.create(AppointmentService.class);
+    }
+
+    private static final class LocalDateAdapter extends TypeAdapter<LocalDate> {
+        @Override
+        public void write(final JsonWriter jsonWriter, final LocalDate localDate) throws IOException {
+            jsonWriter.value(localDate.toString());
+        }
+
+        @Override
+        public LocalDate read(final JsonReader jsonReader) throws IOException {
+            return LocalDate.parse(jsonReader.nextString());
+        }
     }
 }
