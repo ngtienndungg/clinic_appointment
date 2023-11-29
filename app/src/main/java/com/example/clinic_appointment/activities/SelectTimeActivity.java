aab56832269476d3cb57b4bf61ad03f67f67dcf9
ngtienndungg <dungnt.works@gmail.com>
2023-11-29 14:17:45 +0700

    feature: unlogin dialog

--- app/src/main/java/com/example/clinic_appointment/activities/SelectTimeActivity.java
+++ app/src/main/java/com/example/clinic_appointment/activities/SelectTimeActivity.java
@@ -3,17 +3,22 @@ package com.example.clinic_appointment.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
+import com.example.clinic_appointment.R;
 import com.example.clinic_appointment.adapters.SelectTimeAdapter;
 import com.example.clinic_appointment.databinding.ActivitySelectTimeBinding;
+import com.example.clinic_appointment.databinding.DialogNotificationBinding;
 import com.example.clinic_appointment.listeners.AppointmentTimeListener;
 import com.example.clinic_appointment.models.AppointmentTime.AppointmentTime;
 import com.example.clinic_appointment.models.Department.Department;
@@ -21,12 +26,14 @@ import com.example.clinic_appointment.models.Doctor.Doctor;
 import com.example.clinic_appointment.models.HealthFacility.HealthFacility;
 import com.example.clinic_appointment.models.Schedule.DetailSchedule;
 import com.example.clinic_appointment.utilities.Constants;
+import com.example.clinic_appointment.utilities.SharedPrefs;
 
 import java.util.List;
 import java.util.Objects;
 
 public class SelectTimeActivity extends AppCompatActivity implements AppointmentTimeListener {
     private ActivitySelectTimeBinding binding;
+    private final SharedPrefs sharedPrefs = SharedPrefs.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,23 +85,44 @@ public class SelectTimeActivity extends AppCompatActivity implements Appointment
 
     @Override
     public void onClick(AppointmentTime appointmentTime) {
-        if (getIntent().getStringExtra(Constants.KEY_SOURCE_ACTIVITY) == null) {
-            Intent intent = new Intent(this, ConfirmationActivity.class);
-            Doctor selectedDoctor = (Doctor) getIntent().getSerializableExtra(Constants.KEY_DOCTOR);
-            Department selectedDepartment = (Department) getIntent().getSerializableExtra(Constants.KEY_DEPARTMENT);
-            HealthFacility selectedHealthFacility = (HealthFacility) getIntent().getSerializableExtra(Constants.KEY_HEALTH_FACILITY);
-            DetailSchedule selectedSchedule = (DetailSchedule) getIntent().getSerializableExtra(Constants.KEY_DATE);
-            intent.putExtra(Constants.KEY_DATE, selectedSchedule);
-            intent.putExtra(Constants.KEY_DOCTOR, selectedDoctor);
-            intent.putExtra(Constants.KEY_DEPARTMENT, selectedDepartment);
-            intent.putExtra(Constants.KEY_HEALTH_FACILITY, selectedHealthFacility);
-            intent.putExtra(Constants.KEY_TIME, appointmentTime.getTimeNumber());
-            startActivity(intent);
+        Log.d("AccessToken", sharedPrefs.getData(Constants.KEY_ACCESS_TOKEN, String.class));
+        if (Objects.equals(sharedPrefs.getData(Constants.KEY_ACCESS_TOKEN, String.class), "")) {
+            displayDialog();
         } else {
-            Intent intent = new Intent();
-            intent.putExtra(Constants.KEY_TIME, appointmentTime);
-            setResult(RESULT_OK, intent);
-            onBackPressed();
+            if (getIntent().getStringExtra(Constants.KEY_SOURCE_ACTIVITY) == null) {
+                Intent intent = new Intent(this, ConfirmationActivity.class);
+                Doctor selectedDoctor = (Doctor) getIntent().getSerializableExtra(Constants.KEY_DOCTOR);
+                Department selectedDepartment = (Department) getIntent().getSerializableExtra(Constants.KEY_DEPARTMENT);
+                HealthFacility selectedHealthFacility = (HealthFacility) getIntent().getSerializableExtra(Constants.KEY_HEALTH_FACILITY);
+                DetailSchedule selectedSchedule = (DetailSchedule) getIntent().getSerializableExtra(Constants.KEY_DATE);
+                intent.putExtra(Constants.KEY_DATE, selectedSchedule);
+                intent.putExtra(Constants.KEY_DOCTOR, selectedDoctor);
+                intent.putExtra(Constants.KEY_DEPARTMENT, selectedDepartment);
+                intent.putExtra(Constants.KEY_HEALTH_FACILITY, selectedHealthFacility);
+                intent.putExtra(Constants.KEY_TIME, appointmentTime.getTimeNumber());
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent();
+                intent.putExtra(Constants.KEY_TIME, appointmentTime);
+                setResult(RESULT_OK, intent);
+                onBackPressed();
+            }
         }
+    }
+
+    private void displayDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SelectTimeActivity.this);
+        DialogNotificationBinding dialogNotificationBinding = DialogNotificationBinding.inflate(getLayoutInflater());
+        builder.setView(dialogNotificationBinding.getRoot());
+        AlertDialog unLoginDialog = builder.create();
+        if (unLoginDialog.getWindow() != null) {
+            unLoginDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        dialogNotificationBinding.tvTitle.setText(getString(R.string.you_have_not_login_yet));
+        dialogNotificationBinding.tvContent.setText(getString(R.string.this_function_need_to_login_to_use));
+        dialogNotificationBinding.tvAction.setText(getString(R.string.login));
+        dialogNotificationBinding.tvAction.setOnClickListener(v -> startActivity(new Intent(this, LoginActivity.class)));
+        dialogNotificationBinding.ivClose.setOnClickListener(v -> unLoginDialog.dismiss());
+        unLoginDialog.show();
     }
 }
